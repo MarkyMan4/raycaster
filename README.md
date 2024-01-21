56815428d818eb00496ac2cfab4e00479f121dd0
MarkyMan4 <markus.hutnik@gmail.com>
2024-01-21 12:08:45 -0600

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# raycaster
+
+Ray casting and rendering using Go + raylib
+
+![screenshot](screenshot.png)
---
+++ main.go
@@ -0,0 +1,320 @@
+package main
+
+import (
+	"math"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	worldWidth    = 8
+	worldHeight   = 8
+	blockSize     = 64
+	PI2           = math.Pi / 2
+	PI3           = (3 * math.Pi) / 2
+	DR            = 0.01745329251 // approx 1 degree converted to radians
+	maxWallHeight = worldHeight * blockSize
+	FOV           = 60
+)
+
+var world = []int{
+	1, 1, 1, 1, 1, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 0, 0, 0, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 0, 0, 1, 0, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 1,
+	1, 1, 1, 1, 1, 1, 1, 1,
+}
+
+// player data
+var (
+	px        float64 = 256
+	py        float64 = 256
+	pSize     int32   = 10
+	moveSpeed int32   = 3
+	pDeltaX   float64 = 0
+	pDeltaY   float64 = 0
+	pAngle    float64 = 0
+)
+
+func drawWorld() {
+	for y := 0; y < worldHeight; y++ {
+		for x := 0; x < worldWidth; x++ {
+			tileColor := rl.Black
+			if world[y*worldWidth+x] == 1 {
+				tileColor = rl.White
+			}
+
+			rl.DrawRectangle(
+				int32(x*blockSize),
+				int32(y*blockSize),
+				blockSize-1,
+				blockSize-1,
+				tileColor,
+			)
+		}
+	}
+}
+
+func updatePlayer() {
+	rl.DrawRectangle(
+		int32(px),
+		int32(py),
+		pSize,
+		pSize,
+		rl.Green,
+	)
+
+	if rl.IsKeyDown(rl.KeyW) {
+		px += pDeltaX
+		py += pDeltaY
+	}
+	if rl.IsKeyDown(rl.KeyS) {
+		px -= pDeltaX
+		py -= pDeltaY
+	}
+	if rl.IsKeyDown(rl.KeyA) {
+		pAngle -= 0.03
+
+		// reset to 2Pi if go below 0
+		if pAngle < 0 {
+			pAngle += 2 * math.Pi
+		}
+
+		pDeltaX = math.Cos(pAngle) * float64(moveSpeed)
+		pDeltaY = math.Sin(pAngle) * float64(moveSpeed)
+	}
+	if rl.IsKeyDown(rl.KeyD) {
+		pAngle += 0.03
+
+		// reset to 0 if go above 2Pi
+		if pAngle > 2*math.Pi {
+			pAngle -= 2 * math.Pi
+		}
+
+		pDeltaX = math.Cos(pAngle) * float64(moveSpeed)
+		pDeltaY = math.Sin(pAngle) * float64(moveSpeed)
+	}
+}
+
+func checkVerticalLines(angleOffset float64) (float64, float64) {
+	var mapX, mapY, mapPos, depthOfField int
+	var rayX, rayY, rayAngle, xOffset, yOffset float64
+	rayAngle = pAngle + angleOffset
+
+	if rayAngle < 0 {
+		rayAngle += 2 * math.Pi
+	}
+
+	if rayAngle > 2*math.Pi {
+		rayAngle -= 2 * math.Pi
+	}
+
+	aTan := -1.0 / math.Tan(rayAngle)
+
+	// looking up
+	if rayAngle > math.Pi {
+		// round down to the nearest 64th because block size is 64
+		rayY = float64((int(py)>>6)<<6) - 0.0001 // subtract this to make sure ray hits bottom of block
+		rayX = (py-rayY)*aTan + px
+		yOffset = -64
+		xOffset = -yOffset * aTan
+	}
+
+	// looking down
+	if rayAngle < math.Pi {
+		// looking down, round up to the nearest 64th
+		rayY = float64((int(py)>>6)<<6) + 64
+		rayX = (py-rayY)*aTan + px
+		yOffset = 64
+		xOffset = -yOffset * aTan
+	}
+
+	// looking straight left or right (will never hit a horizontal line)
+	if rayAngle == 0 || rayAngle == math.Pi {
+		rayX = px
+		rayY = py
+		depthOfField = 8
+	}
+
+	for depthOfField < 8 {
+		// bitshift is divide by 64
+		mapX = int(rayX) >> 6
+		mapY = int(rayY) >> 6
+		mapPos = mapY*worldWidth + mapX
+
+		// hit wall
+		if mapPos > 0 && mapPos < worldWidth*worldHeight && world[mapPos] == 1 {
+			depthOfField = 8
+		} else {
+			rayX += xOffset
+			rayY += yOffset
+			depthOfField++
+		}
+	}
+
+	return rayX, rayY
+}
+
+func checkHorizontalLines(angleOffset float64) (float64, float64) {
+	var mapX, mapY, mapPos, depthOfField int
+	var rayX, rayY, rayAngle, xOffset, yOffset float64
+	rayAngle = pAngle + angleOffset
+
+	if rayAngle < 0 {
+		rayAngle += 2 * math.Pi
+	}
+
+	if rayAngle > 2*math.Pi {
+		rayAngle -= 2 * math.Pi
+	}
+
+	nTan := -math.Tan(rayAngle)
+
+	// looking left
+	if rayAngle > PI2 && rayAngle < PI3 {
+		// round down to the nearest 64th because block size is 64
+		rayX = float64((int(px)>>6)<<6) - 0.0001 // subtract this to make sure ray hits bottom of block
+		rayY = (px-rayX)*nTan + py
+		xOffset = -64
+		yOffset = -xOffset * nTan
+	}
+
+	// looking right
+	if rayAngle < PI2 || rayAngle > PI3 {
+		// looking down, round up to the nearest 64th
+		rayX = float64((int(px)>>6)<<6) + 64
+		rayY = (px-rayX)*nTan + py
+		xOffset = 64
+		yOffset = -xOffset * nTan
+	}
+
+	// looking straight up or down
+	if rayAngle == PI2 || rayAngle == PI3 {
+		rayX = px
+		rayY = py
+		depthOfField = 8
+	}
+
+	for depthOfField < 8 {
+		// bitshift is divide by 64
+		mapX = int(rayX) >> 6
+		mapY = int(rayY) >> 6
+		mapPos = mapY*worldWidth + mapX
+
+		// hit wall
+		if mapPos > 0 && mapPos < worldWidth*worldHeight && world[mapPos] == 1 {
+			depthOfField = 8
+		} else {
+			rayX += xOffset
+			rayY += yOffset
+			depthOfField++
+		}
+	}
+
+	return rayX, rayY
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	x := math.Pow(x1-x2, 2)
+	y := math.Pow(y1-y2, 2)
+
+	return math.Sqrt(x + y)
+}
+
+func minFloat(val1, val2 float64) float64 {
+	if val1 <= val2 {
+		return val1
+	}
+
+	return val2
+}
+
+func castRays() {
+	for i := 0.0; i < FOV; i += 0.25 {
+		degrees := (i - (FOV / 2)) * DR
+		ray1X, ray1Y := checkVerticalLines(degrees)
+		ray2X, ray2Y := checkHorizontalLines(degrees)
+
+		// pick the shortest ray
+		rayX := ray1X
+		rayY := ray1Y
+		dist1 := distance(px, py, ray1X, ray1Y)
+		dist2 := distance(px, py, ray2X, ray2Y)
+		dist := dist1
+		color := rl.SkyBlue
+
+		if dist2 < dist1 {
+			rayX = ray2X
+			rayY = ray2Y
+			dist = dist2
+			color = rl.Blue
+		}
+
+		rl.DrawLine(
+			int32(px),
+			int32(py),
+			int32(rayX),
+			int32(rayY),
+			rl.Red,
+		)
+
+		// render in 3D
+
+		// fix fisheye
+		rayAngle := pAngle + degrees
+		if rayAngle < 0 {
+			rayAngle += 2 * math.Pi
+		}
+
+		if rayAngle > 2*math.Pi {
+			rayAngle -= 2 * math.Pi
+		}
+
+		cameraAngle := pAngle - (pAngle + degrees)
+		if cameraAngle < 0 {
+			cameraAngle += 2 * math.Pi
+		}
+
+		if cameraAngle > 2*math.Pi {
+			cameraAngle -= 2 * math.Pi
+		}
+
+		dist = dist * math.Cos(cameraAngle)
+
+		wallHeight := math.Min((blockSize*maxWallHeight)/dist, maxWallHeight)
+		wallOffset := int32((maxWallHeight / 2) - (wallHeight / 2))
+
+		rl.DrawRectangle(
+			int32(i*8+530),
+			wallOffset,
+			2,
+			int32(wallHeight),
+			color,
+		)
+	}
+}
+
+func main() {
+	rl.InitWindow(1024, 512, "raycaster")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	// initialize player by calculating initial delta x and y
+	pDeltaX = math.Cos(pAngle) * float64(moveSpeed)
+	pDeltaY = math.Sin(pAngle) * float64(moveSpeed)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.DarkGray)
+
+		drawWorld()
+		updatePlayer()
+		castRays()
+
+		rl.EndDrawing()
+	}
+}
